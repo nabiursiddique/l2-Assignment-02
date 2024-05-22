1bc081dd4e9a85e9698cbf94b462393e22d1f8e9
nabiur siddique <nabiursiddique01@gmail.com>
2024-05-22 15:11:48 +0600

    now we can create order in the db

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import express, { Request, Response } from 'express';
 export const app = express();
 import cors from 'cors';
 import { ProductRoutes } from './modules/product/product.route';
+import { OrderRoutes } from './modules/order/order.route';
 
 app.use(express.json());
 app.use(cors());
 
 app.use('/api/products', ProductRoutes);
+app.use('/api/orders', OrderRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
--- src/modules/order/order.model.ts
+++ src/modules/order/order.model.ts
@@ -4,11 +4,11 @@ import { TOrder } from './order.interface';
 const orderSchema = new Schema<TOrder>(
   {
     email: { type: String, required: true },
-    productId: { type: String, required: true },
+    productId: { type: String, required: true, unique: true },
     price: { type: Number, required: true },
     quantity: { type: Number, required: true },
   },
-  { versionKey: false, _id: false },
+  { versionKey: false },
 );
 
 export const Order = mongoose.model<TOrder>('Order', orderSchema);
